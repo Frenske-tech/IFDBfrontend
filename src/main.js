d289c07fcab6d6e477dcaa13b1d4257e9496ecad
Frenske-tech <461403@student.fontys.nl>
2023-11-14 11:00:44 +0100

    added firebase

--- src/main.js
+++ src/main.js
@@ -7,12 +7,31 @@ import List from './components/pages/List';
 import Create from './components/pages/Create';
 import Edit from './components/pages/Edit';
 import Show from './components/pages/Show';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
   
 axios.defaults.baseURL = "http://localhost:8080/";
-// axios.interceptors.request.use(function (config) {
-// //   config.headers['X-Binarybox-Api-Key'] = process.env.VUE_APP_API_KEY;
-// //   return config;
-// });
+const firebaseConfig = {
+  apiKey: "AIzaSyDkSaX72CYVnn9nxOk3b-PxNBFG4mxz91E",
+
+  authDomain: "ifdb-59765.firebaseapp.com",
+
+  projectId: "ifdb-59765",
+
+  storageBucket: "ifdb-59765.appspot.com",
+
+  messagingSenderId: "521229900403",
+
+  appId: "1:521229900403:web:3577735dfaa5bf773e2806",
+
+  measurementId: "G-KSMM7J2DNF"
+
+};
+
+const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
   
 const router = createRouter({
   history: createWebHistory(),
